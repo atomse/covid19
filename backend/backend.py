7c5101c1d0d0d899b6e196ab7afbccbaabeaca31
Sky Zhang <sky.atomse@gmail.com>
2020-11-11 11:26:11 +0800

    fire update

--- backend/backend.py
+++ backend/backend.py
@@ -1,16 +1,23 @@
-from flask import Flask
-from flask import request, jsonify
-import pandas as pd
-from datetime import datetime
-import dateutil.parser
+"""
+
+This is the backend of Covid19 visualization project
+
+
+"""
+
 import threading
 import time
+from datetime import datetime
+import dateutil.parser
+from flask import Flask
+from flask import request
+import pandas as pd
 
 app = Flask(__name__)
 
-apiHead = '/api/v1'
+API_HEAD = '/api/v1'
 
-global df
+global COVID19_DF
 
 
 """
@@ -19,28 +26,37 @@ Reload / checkUpdate
 """
 
 
-def reloadDataFrame():
-    # global df
-    df = pd.read_csv("WHO-COVID-19-global-data.csv")
-    df.columns = [_.strip() for _ in df.columns.values]
-    return df
+def reload_data_frame():
+    """
+    Reload DataFrame df from WHO-COVID-19-global-data.csv
+    """
+    covid19_df = pd.read_csv("WHO-COVID-19-global-data.csv")
+    covid19_df.columns = [_.strip() for _ in covid19_df.columns.values]
+    return covid19_df
 
 
-df = reloadDataFrame()
+COVID19_DF = reload_data_frame()
 
 
-def isLatestData():
-    global df
+def is_latest_data():
+    """
+    Check if the df is the lastest by checking if latest date if the
+    utc now date
+    """
+    global COVID19_DF
     utcnow = datetime.utcnow()
-    dataLast = max(df.Date_reported)
-    dataLast = dateutil.parser.parse(dataLast)
-    if utcnow.year == dataLast.year and utcnow.month == dataLast.month:
-        if abs(dataLast.day - utcnow.day) <= 0:
+    data_last = max(COVID19_DF.Date_reported)
+    data_last = dateutil.parser.parse(data_last)
+    if utcnow.year == data_last.year and utcnow.month == data_last.month:
+        if abs(data_last.day - utcnow.day) <= 0:
             return True
     return False
 
 
-def downloadData():
+def download_data():
+    """
+    docs
+    """
     if app.debug:
         print("Updating Data")
     import requests
@@ -51,29 +67,39 @@ def downloadData():
         fd.write(text)
 
 
-def checkUpdate():
-    global df
-    if not isLatestData():
-        downloadData()
-        df = reloadDataFrame()
+def check_update():
+    """
+    docs
+    """
+    global COVID19_DF
+    if not is_latest_data():
+        download_data()
+        COVID19_DF = reload_data_frame()
         return True
     return False
     # return pd.read_csv("WHO-COVID-19-global-data.csv")
 
 
-def checkUpdateHourly():
+def check_update_hourly():
+    """
+    docs
+    """
     while True:
-        checkUpdate()
+        check_update()
         time.sleep(600)
 
 
 class CheckUpdateThread(threading.Thread):
+    """
+    CheckUpdateThread
+    """
+
     def run(self):
-        checkUpdateHourly()
+        check_update_hourly()
 
 
-AllCountries = list(set(df.Country))
-updateThread = CheckUpdateThread()
+ALL_COUNTRIES = list(set(COVID19_DF.Country))
+UPDATE_THREAD = CheckUpdateThread()
 
 """
 
@@ -83,37 +109,43 @@ Get Data
 """
 
 
-def getCovid19Data(startDate=None, endDate=None, country=None):
-    global df
-    tempdf = df
-    importantCounties = ['China', 'United States of America']
+def get_covid19_data(start_date=None, end_date=None, country=None):
+    """
+    docs
+    """
+    global COVID19_DF
+    temp_df = COVID19_DF
+    important_counties = ['China', 'United States of America']
     if country is None:
-        country = importantCounties
+        country = important_counties
     elif isinstance(country, str):
         country = [country]
-    assert all([_ in AllCountries for _ in country]), 'Invalid country name'
-    if startDate and endDate:
-        assert startDate <= endDate
+    assert all([_ in ALL_COUNTRIES for _ in country]), 'Invalid country name'
+    if start_date and end_date:
+        assert start_date <= end_date
     if country:
-        tempdf = tempdf[tempdf.Country.isin(country)]
-    if startDate:
-        tempdf = tempdf[tempdf.Date_reported >= startDate]
-    if endDate:
-        tempdf = tempdf[tempdf.Date_reported <= endDate]
+        temp_df = temp_df[temp_df.Country.isin(country)]
+    if start_date:
+        temp_df = temp_df[temp_df.Date_reported >= start_date]
+    if end_date:
+        temp_df = temp_df[temp_df.Date_reported <= end_date]
     items = ['New_cases', 'Cumulative_cases',
              'New_deaths', 'Cumulative_deaths']
-    xData = tempdf.Date_reported.tolist()
-    yData = {}
+    x_data = temp_df.Date_reported.tolist()
+    y_data = {}
     for i in items:
-        yData[i] = tempdf[i].tolist()
+        y_data[i] = temp_df[i].tolist()
     response = {
-        'xData': xData,
-        'yData': yData
+        'xData': x_data,
+        'yData': y_data
     }
     return response
 
 
-def packData(code, msg, data):
+def pack_data(code, msg, data):
+    """
+    docs
+    """
     return {
         'code': code,
         'msg': msg,
@@ -121,55 +153,64 @@ def packData(code, msg, data):
     }
 
 
-"""
+@app.route(API_HEAD + '/allCountries')
+def all_countries():
+    """
+    docs
+    """
+    return pack_data(20000, 'success', ALL_COUNTRIES)
 
 
-Routers
+@app.route(API_HEAD + '/allDate')
+def all_date():
+    """
+    docs
+    """
+    global COVID19_DF
+    return pack_data(20000, 'success', sorted(list(set(COVID19_DF.Date_reported))))
 
 
-"""
-
-
-@app.route(apiHead + '/allCountries')
-def allCountries():
-    return packData(20000, 'success', AllCountries)
-
-
-@app.route(apiHead + '/allDate')
-def allDate():
-    global df
-    return packData(20000, 'success', sorted(list(set(df.Date_reported))))
-
-
-@app.route(apiHead + '/dateRange')
-def dateRange():
-    global df
-    return packData(20000, 'success', {
-        'min': min(df.Date_reported),
-        'max': max(df.Date_reported)
+@app.route(API_HEAD + '/dateRange')
+def date_range():
+    """
+    docs
+    """
+    global COVID19_DF
+    return pack_data(20000, 'success', {
+        'min': min(COVID19_DF.Date_reported),
+        'max': max(COVID19_DF.Date_reported)
     })
 
 
-@app.route(apiHead + '/covid19')
+@app.route(API_HEAD + '/covid19')
 def covid19():
+    """
+    docs
+    """
     """
     Using restful
     """
-    reqData = request.args or {}
+    req_data = request.args or {}
     # import pdb; pdb.set_trace()
-    covidData = getCovid19Data(reqData.get("startDate", None), reqData.get(
-        "endDate", None), reqData.get("country", None))
-    return packData(20000, 'success', covidData)
+    covid_data = get_covid19_data(req_data.get("startDate", None), req_data.get(
+        "endDate", None), req_data.get("country", None))
+    return pack_data(20000, 'success', covid_data)
 
 
 @app.route('/user/login', methods=['GET', 'POST'])
 def login():
-    return packData(20000, 'success', {"token": "admin-token"})
+    """
+    docs
+    """
+    return pack_data(20000, 'success', {"token": "admin-token"})
 
 
 @app.route('/user/info', methods=['GET', 'POST'])
 def userinfo():
-    return packData(20000, 'success', {
+    """
+    docs
+    """
+    return pack_data(20000, 'success', {
         "avatar": "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
         "introduction": "I am a super administrator",
         "name": "Super Admin",
@@ -179,9 +220,12 @@ def userinfo():
 
 @app.route('/user/logout', methods=['GET', 'POST'])
 def logout():
-    return packData(20000, 'success', 'success')
+    """
+    docs
+    """
+    return pack_data(20000, 'success', 'success')
 
 
 if __name__ == '__main__':
-    updateThread.start()
+    UPDATE_THREAD.start()
     app.run(port=8080, debug=True)
